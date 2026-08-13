53b1e6c222f372e6d4a1b73580ae1dd0834c4d7a
Zoe <zoe.arbuckle@me.com>
2026-08-13 15:23:22 -0400

    add message validators

---
+++ GenericNotificationSystem/Validators/PushValidator.cs
@@ -0,0 +1,13 @@
+﻿using GenericNotificationSystem.Models;
+
+namespace GenericNotificationSystem.Validators
+{
+    public class PushValidator : MessageValidator<PushMessage>
+    {
+        protected override IEnumerable<string> ValidateMessage(PushMessage message)
+        {
+            if (string.IsNullOrWhiteSpace(message.DeviceToken))
+                yield return "Device Token is required";
+        }
+    }
+}

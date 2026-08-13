e7c43816a977b988f6d4db199cfdd4025ce78d60
Zoe <zoe.arbuckle@me.com>
2026-08-13 15:01:00 -0400

    add generic messageReader and messageHandler

---
+++ GenericNotificationSystem/Handlers/IMessageReader.cs
@@ -0,0 +1,9 @@
+﻿using GenericNotificationSystem.Models;
+
+namespace GenericNotificationSystem.Handlers
+{
+    public interface IMessageReader<out T> where T : IMessage
+    {
+        T ReadNext();
+    }
+}
---
+++ GenericNotificationSystem/Handlers/UniversalHandler.cs
@@ -0,0 +1,14 @@
+﻿using GenericNotificationSystem.Models;
+
+namespace GenericNotificationSystem.Handlers
+{
+    public class UniversalHandler : IMessageHandler<IMessage>
+    {
+        public DeliveryResult Handle(IMessage message)
+        {
+            // Replace with delivery logic
+            Console.WriteLine($"Handling message {message.GetType()}: {message.Subject}");
+            return new DeliveryResult(true, "Universal");
+        }
+    }
+}

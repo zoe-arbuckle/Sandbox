24710489755a05f9e9ee53dd123262dbef32aa82
Zoe <zoe.arbuckle@me.com>
2026-08-13 15:07:50 -0400

    add notification builder

---
+++ GenericNotificationSystem/Builders/EmailNotificationBuilder.cs
@@ -0,0 +1,30 @@
+﻿using GenericNotificationSystem.Models;
+
+namespace GenericNotificationSystem.Builders
+{
+    public class EmailNotificationBuilder : NotificationBuilder<EmailNotificationBuilder, EmailMessage>
+    {
+        private string? _recipient;
+        private string? _body;
+
+        public EmailNotificationBuilder WithRecipient(string recipient)
+        {
+            _recipient = recipient;
+            return this;
+        }
+
+        public EmailNotificationBuilder WithBody(string body)
+        {
+            _body = body;
+            return this;
+        }
+
+        public override EmailMessage Build()
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(_recipient, nameof(_recipient));
+            ArgumentException.ThrowIfNullOrWhiteSpace(_body, nameof(_body));
+
+            return new EmailMessage(_subject, _recipient, _body);
+        }
+    }
+}
---
+++ GenericNotificationSystem/Builders/NotificationBuilder.cs
@@ -0,0 +1,19 @@
+﻿using GenericNotificationSystem.Models;
+
+namespace GenericNotificationSystem.Builders
+{
+    public abstract class NotificationBuilder<TBuilder, TMessage>
+        where TBuilder : NotificationBuilder<TBuilder, TMessage>
+        where TMessage : IMessage
+    {
+        protected string _subject = string.Empty;
+
+        public TBuilder WithSubject(string subject)
+        {
+            _subject = subject;
+            return (TBuilder)this;
+        }
+
+        public abstract TMessage Build();
+    }
+}

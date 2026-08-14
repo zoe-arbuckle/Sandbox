686b68833bd54f390d536d5a8863693b47c20837
Zoe <zoe.arbuckle@me.com>
2026-08-13 20:50:01 -0400

    add LINQ project and examples

---
+++ LINQ/Program.cs
@@ -0,0 +1,52 @@
+﻿using Common;
+
+/*
+ * =====================================
+ * LINQ Basics & Query Syntax
+ * =====================================
+ */
+CustomConsoleLogs.Section("LINQ Basics & Query Syntax");
+List<int> integers = [10, 25, 8, 42, 15, 33, 7];
+// Define the query expression
+IEnumerable<int> numbersGreaterThan15 =
+    from integer in integers
+    where integer > 15
+    select integer;
+
+// Execute the query
+foreach (var i in numbersGreaterThan15)
+{
+    Console.WriteLine(i);
+}
+
+/*
+ * =====================================
+ * Select & Transformation
+ * =====================================
+ */
+CustomConsoleLogs.Section("Select & Transformation");
+
+List<string> names = new List<string> { "Alice", "Bob", "Charlie", "Diana" };
+
+// create a new collection where each name is transformed to uppercase
+IEnumerable<string> asUppercase = from name in names
+                                  select name.ToUpper();
+// create a new collection with anonymous objects containing original name and string length
+var transformed = from name in names
+                  select new { Original = name, Length = name.Length };
+
+// print results
+Console.WriteLine("Uppercase:");
+foreach (var name in asUppercase)
+    Console.WriteLine(name);
+
+Console.WriteLine("Transformed:");
+foreach (var obj in transformed)
+    Console.WriteLine($"{obj.Original} length is {obj.Length}");
+
+/*
+ * =====================================
+ * Combining Where & Select (Chaining)
+ * =====================================
+ */
+CustomConsoleLogs.Section("Combining Where & Select (Chaining)");

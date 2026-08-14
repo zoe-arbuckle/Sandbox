47580c27cd6de7a5fbb4305c24aaef6be6f4bef7
Zoe <zoe.arbuckle@me.com>
2026-08-13 21:05:30 -0400

    add combination where and select example

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -50,3 +50,21 @@ foreach (var obj in transformed)
  * =====================================
  */
 CustomConsoleLogs.Section("Combining Where & Select (Chaining)");
+
+List<int> prices = new List<int> { 5, 15, 8, 25, 12, 30, 10 };
+
+var discountedPrices = from price in prices
+                       where price >= 10 // filter for prices greater than or equal to 10
+                       select new { OriginalPrice = price, DiscountedPrice = price * 0.8 }; // apply a 20% discount to each price & create objects with OriginalPrice and DiscountedPrice
+
+// print results
+Console.WriteLine("Discounted Prices:");
+foreach (var price in discountedPrices)
+    Console.WriteLine($"{price.OriginalPrice} discounted to {price.DiscountedPrice}");
+
+/*
+ * =====================================
+ * FirstOrDefault, SingleOrDefault & Aggregation
+ * =====================================
+ */
+CustomConsoleLogs.Section("FirstOrDefault, SingleOrDefault & Aggregation");

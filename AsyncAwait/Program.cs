7498d7b6c0e2cf0f99a27f42012b0c81d989ceb6
Zoe <zoe.arbuckle@me.com>
2026-08-20 14:00:58 -0400

    add basic example to program.cs

--- AsyncAwait/Program.cs
+++ AsyncAwait/Program.cs
@@ -1,8 +1,10 @@
 ﻿using AsyncAwait;
+
 /*
  * =====================================
  * Basics of Async/Await
  * =====================================
  */
-
 Common.CustomConsoleLogs.Section("Basics of Async/Await");
+Lesson1_AsyncAwaitBasics basics = new Lesson1_AsyncAwaitBasics();
+await basics.FetchMultipleUsersSequentialAsync();

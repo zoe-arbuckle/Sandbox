0aeb1c401bac168bfbe31ed6db30ef6f083948a8
Zoe <zoe.arbuckle@me.com>
2026-08-20 13:57:41 -0400

    add async await project and basics lesson

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,8 @@
+﻿using AsyncAwait;
+/*
+ * =====================================
+ * Basics of Async/Await
+ * =====================================
+ */
+
+Common.CustomConsoleLogs.Section("Basics of Async/Await");
